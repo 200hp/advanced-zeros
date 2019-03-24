f3a16a4ba98b52e759265c15ff916cb070ec97fc
200hp <qwertyqw8@mail.ru>
2019-03-24 03:09:23 +0300

    feat: implement the getZerosCount function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  var result = Number.MAX_VALUE;
+  
+  for(var i = 2; i <= base; i++) {
+    var p = 0;
+    while (base % i == 0) {
+      p++;
+      base /= i;
+    }
+    var ndevi = Math.floor(number / i);
+    var n = 0;
+    while (ndevi > 0) {
+      n += ndevi;
+      ndevi = Math.floor(ndevi / i);
+    }
+    result = Math.min(result, Math.floor(n / p));
+  }
+  return result;
 }
